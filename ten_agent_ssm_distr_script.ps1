75da3bc79f98b84f7b72c434ef0d362ddb118359
Ceeya <INTERIM-CORE-SHARED-CODECOMMIT-REPOUSER-SVC-at-687108392003>
2025-03-13 12:12:19 +0200

    updated a number of files

--- ten_agent_ssm_distr_script.ps1
+++ ten_agent_ssm_distr_script.ps1
@@ -1,7 +1,7 @@
 cd c:\;
 mkdir temp;
 cd temp;
-Read-S3Object -BucketName ct-ire-poc1-tenable-agent -Key windows/NessusAgent-10.1.4-x64.msi -File NessusAgent-10.1.4-x64.msi;
+Read-S3Object -BucketName <bucket_name> -Key windows/NessusAgent-10.1.4-x64.msi -File NessusAgent-10.1.4-x64.msi;
 Start-Process msiexec.exe -Wait -ArgumentList '/I NessusAgent-10.1.4-x64.msi /quiet';
 cd "C:\Program Files\Tenable\Nessus Agent";
-.\"nessuscli.exe" agent link --key=7f26d3482e7afa2a3147259e3a8f5bceab19a62d4a693ea9e1114d0edd483297 --host=sensor.cloud.tenable.com --port=443
+.\"nessuscli.exe" agent link --key=<your_key> --host=<your_host> --port=443
